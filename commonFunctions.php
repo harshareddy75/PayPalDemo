a737d0d67cd9f79794eff99d81d1bf558745a4ee
Harsha <harshareddy75@gmail.com>
2015-05-19 03:20:44 +0000

    commenting curl verify peer false

--- commonFunctions.php
+++ commonFunctions.php
@@ -21,7 +21,7 @@
 	{
 		$curl = curl_init();
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+//		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 		curl_setopt($curl, CURLOPT_POST, true);
 		curl_setopt($curl, CURLOPT_URL, 'https://api-3t.sandbox.paypal.com/nvp');
 		curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($parameters));
